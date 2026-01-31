6c47bf63ced13ba89328d3cabaa6a6dce2e52d7b
Sayani Maity <sayanimaity2024@gmail.com>
2026-02-01 02:51:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+$(document).ready(function() {
+    // Interactive Ripple Effect
+    try {
+        $('#home').ripples({
+            resolution: 512,
+            dropRadius: 20,
+            perturbance: 0.04,
+        });
+    } catch (e) {
+        console.error("Ripple effect failed to load", e);
+    }
 
+    // Smooth Scrolling
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
